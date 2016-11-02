74fe8bb26def3250c4359a754d97f3975f9c0530
Siva <siva.bonigeni@gmail.com>
2016-11-02 19:19:36 +0530

    Initial Commit

---
+++ KeepWorksTaskTests/KeepWorksTaskTests.m
@@ -0,0 +1,39 @@
+//
+//  KeepWorksTaskTests.m
+//  KeepWorksTaskTests
+//
+//  Created by Satish on 11/2/16.
+//  Copyright © 2016 Satish. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KeepWorksTaskTests : XCTestCase
+
+@end
+
+@implementation KeepWorksTaskTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
